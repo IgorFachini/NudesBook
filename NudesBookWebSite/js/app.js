850a19f61e272eb37f6a7b9615d6e49da911d73a
igorfachini <igor.fachini@catolicasc.org.br>
2017-02-14 22:50:06 -0200

    Organizado pastas e adicionado novo repositorio

--- NudesBookWebSite/js/app.js
+++ NudesBookWebSite/js/app.js
@@ -1,8 +1,8 @@
 var config = {
-    apiKey: "AIzaSyBeJCvaU6Veug2PNLRHCB_7OVfpVq-u5qc",
-    authDomain: "curso-fb-ef545.firebaseapp.com",
-    databaseURL: "https://curso-fb-ef545.firebaseio.com",
-    storageBucket: "curso-fb-ef545.appspot.com",
-    messagingSenderId: "633758739974"
+  apiKey: "AIzaSyCY0aoFiZxxrSXTbbBwrNxeGtIbU2QPYpU",
+  authDomain: "nudesbook.firebaseapp.com",
+  databaseURL: "https://nudesbook.firebaseio.com",
+  storageBucket: "nudesbook.appspot.com",
+  messagingSenderId: "48124758803"
 };
 firebase.initializeApp(config);
